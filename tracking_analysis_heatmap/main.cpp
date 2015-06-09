821de317afbe914b959656e388b573b117416a35
konovagit <corentincorde@orange.fr>
2015-06-09 22:08:08 +0100

    First commit, install library and header for opencv

--- tracking_analysis_heatmap/main.cpp
+++ tracking_analysis_heatmap/main.cpp
@@ -1,15 +1,48 @@
-//
-//  main.cpp
-//  tracking_analysis_heatmap
-//
-//  Created by konova on 09/06/2015.
-//  Copyright (c) 2015 konova. All rights reserved.
-//
+#include <stdio.h>
+#include "opencv/highgui.h"
+#include "opencv/cv.h"
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // Touche clavier
+    char key;
+    // Image
+    IplImage *image;
+    // Capture vidéo
+    CvCapture *capture;
+    
+    // Ouvrir le flux vidéo
+    //capture = cvCreateFileCapture("/path/to/your/video/test.avi"); // chemin pour un fichier
+    capture = cvCreateCameraCapture(CV_CAP_ANY);
+    
+    // Vérifier si l'ouverture du flux est ok
+    if (!capture) {
+        
+        printf("Ouverture du flux vidéo impossible !\n");
+        return 1;
+        
+    }
+    
+    // Définition de la fenêtre
+    cvNamedWindow("GeckoGeek Window", CV_WINDOW_AUTOSIZE);
+    
+    // Boucle tant que l'utilisateur n'appuie pas sur la touche q (ou Q)
+    while(key != 'q' && key != 'Q') {
+        
+        // On récupère une image
+        image = cvQueryFrame(capture);
+        
+        // On affiche l'image dans une fenêtre
+        cvShowImage( "GeckoGeek Window", image);
+        
+        // On attend 10ms
+        key = cvWaitKey(10);
+        
+    }
+    
+    cvReleaseCapture(&capture);
+    cvDestroyWindow("GeckoGeek Window");
+    
     return 0;
+    
 }
