736cdd57611b3d170939f0abe36f26cb94dfbc4b
konovagit <corentincorde@orange.fr>
2015-06-10 14:20:22 +0100

    code for create histogram (http://docs.opencv.org/doc/tutorials/imgproc/histograms/histogram_calculation/histogram_calculation.html)

--- tracking_analysis_heatmap/main.cpp
+++ tracking_analysis_heatmap/main.cpp
@@ -23,6 +23,7 @@ void generate_heatmap(string filename, double begin, double end);
 
 void conversion_grayscale(string png_name);
 
+
 IplImage* histogram(IplImage *image);
 
 
@@ -38,32 +39,74 @@ int main()
     
     generate_heatmap("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/134823_Feng_coordinates.txt", 5, 439);
     string imageName = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png";
-    string imageName2 = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap9.png";
+    //string imageName2 = "/Users/konova/tracking_analysis_heatmap/Res/lena.png";
     
     
-    conversion_grayscale(imageName2);
+    //conversion_grayscale(imageName2);
     
-
-    IplImage *hist, *image;
-    image = cvLoadImage("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap2.png");
-    hist=histogram(image);
+    Mat src, dst;
     
-    char key;
+    /// Load image
+    src = imread( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap9.png", 1 );
     
-    cvShowImage("difference between Two heatmap", hist);
+    if( !src.data )
+    { return -1; }
     
-    // While we don't want to quit
-    while(key != 'Q' && key != 'q') {
-        
-        key = cvWaitKey(10);
+    /// Separate the image in 3 places ( B, G and R )
+    vector<Mat> bgr_planes;
+    split( src, bgr_planes );
+    
+    /// Establish the number of bins
+    int histSize = 256;
+    
+    /// Set the ranges ( for B,G,R) )
+    float range[] = { 0, 256 } ;
+    const float* histRange = { range };
+    
+    bool uniform = true; bool accumulate = false;
+    
+    Mat b_hist, g_hist, r_hist, gray_hist;
+    
+    /// Compute the histograms:
+    calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+    
+    // Draw the histograms for B, G and R
+    int hist_w = 512; int hist_h = 400;
+    int bin_w = cvRound( (double) hist_w/histSize );
+    
+    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+    
+    /// Normalize the result to [ 0, histImage.rows ]
+    
+    normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    
+    /// Draw for each channel
+    for( int i = 1; i < histSize; i++ )
+    {
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+             Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+             Scalar( 255, 0, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+             Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+             Scalar( 0, 255, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+             Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+             Scalar( 0, 0, 255), 2, 8, 0  );
         
     }
-    // Destroy the window we have created
-    cvDestroyWindow("difference between Two heatmap");
     
+    /// Display
+    namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
+    imshow("calcHist Demo", histImage );
     
+    waitKey(0);
     
     return 0;
+
 }
 
 void conversion_grayscale(string png_name)
@@ -191,55 +234,6 @@ void generate_heatmap(string filename, double begin, double end)
 }
 
 
-IplImage* histogram(IplImage *image)
-{
-    //Function which takes an image as parameter and returns the image containing the gray-scale intensity histogram
-    
-    IplImage* imgHistogram; IplImage* gray; //Variables for the histogram's image and the gray-scale intermediate
-    CvHistogram* hist; //To store the histogram variable in the proper format
-    
-    //The next few lines of declarations are all important variables for executing the OpenCV histogram functions
-    
-    int bins = 256;//Size of the histogram
-    int hsize[] = {bins}; //One-dimensional histogram, so one value in array
-    
-    float max_value = 0, min_value = 0; //Max and min value of the histogram
-    
-    int value; //Value of frequency;
-    int normalized, scale = 200; //Variables for storing the normalized value, and the scaled maximum (200)
-    
-    float xranges[] = {0,255};//Ranges of the values
-    float* ranges[] = {xranges};//One-dimensional image, so only one value in array
-    
-    gray = cvCreateImage(cvGetSize(image), 8, 1);
-    cvCvtColor(image, gray, CV_BGR2GRAY);//Grayscale equivalent created
-    
-    IplImage* planes[] = {gray};//Planes to obtain the histogram, in this case just one
-    
-    hist = cvCreateHist(1, hsize, CV_HIST_ARRAY, ranges,1);//Creates the histogram with the necessary parameters
-    cvCalcHist(planes, hist, 0, NULL); //Calculates the histogram with the parameters
-    cvGetMinMaxHistValue(hist, &min_value, &max_value); //Reports the minimal and maximal values in histogram
-    
-    imgHistogram = cvCreateImage(cvSize(bins,scale),8,3); //Creates the image of the histogram of width 'bins' (256) and height 'scale' (200)
-    CvScalar CV_BGR(255,255,255), CV_BGR1(0,0,0);
-    
-    
-    cvRectangle(imgHistogram, cvPoint(0,0), cvPoint(bins-1,scale-1), CV_BGR,-1); //Draws a white rectangle from (0,0) to (256,200)
-    
-    //Draws the vertically flipped histogram
-    
-    for(int i=0; i < bins; i++)
-   	{
-        value = cvGetReal1D(hist->bins, i); //Obtains the frequency for the intensity i
-        normalized = cvRound(value*scale/max_value); //Scales it down
-        cvLine(imgHistogram,cvPoint(i,scale), cvPoint(i,scale-normalized), CV_BGR1); //Draws a line in the vertical direction of that height from point (i,200) to (i,200-normalized)
-    }
-    
-    cvFlip(image, NULL, 1);// flips the image upside down
-    
-    cvReleaseImage(&gray);
-    return imgHistogram; //Returns the image
-}
 
 
 
