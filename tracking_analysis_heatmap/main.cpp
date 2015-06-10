67b541c433aa279ad8e33fa8ec1ab78d372dd0c9
konovagit <corentincorde@orange.fr>
2015-06-10 15:10:42 +0100

    delete useless function for his branch

--- tracking_analysis_heatmap/main.cpp
+++ tracking_analysis_heatmap/main.cpp
@@ -21,29 +21,11 @@ using namespace cv;
 
 
 void create_histogram(string path_save, string image);
-void generate_heatmap(string filename, double begin, double end);
-
-void conversion_grayscale(string png_name);
-
-IplImage* histogram(IplImage *image);
-
-double getPSNR(const Mat& I1, const Mat& I2); //for the comparison
 
 
 int main()
 {
-    string vide_name="134823_Feng";
-    string coordinates="../Res/Video_" + vide_name + "/" + vide_name + "_coordinates.txt";
-    cout<<"------generation heatmas----"<<endl;
-    cout<<"close windows to continue" <<endl;
-    
-    
-    generate_heatmap("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/134823_Feng_coordinates.txt", 5, 439);
-    string imageName = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png";
-    string imageName2 = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap9.png";
-    
-    
-    conversion_grayscale(imageName2);
+
     
     create_histogram("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/histogramme.png", "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png");
     
@@ -52,19 +34,6 @@ int main()
 }
 
 
-void conversion_grayscale(string png_name)
-{
-    Mat image = imread(png_name, CV_LOAD_IMAGE_COLOR);
-    if(! image.data )
-    {
-        cout <<  "Could not open or find the image" << endl ;
-    }
-    // Create a new matrix to hold the gray image
-    Mat gray;
-    // convert RGB image to gray
-    cvtColor(image, gray, CV_BGR2GRAY);
-    imwrite( png_name, gray );
-}
 
 
 void create_histogram(string path_save, string image)
@@ -139,113 +108,6 @@ void create_histogram(string path_save, string image)
 
 
 
-void generate_heatmap(string filename, double begin, double end)
-{
-    // To write on specific files for each couple (begin,end).
-    string b = to_string((int)begin), e = to_string((int)end);
-    
-    // Path of the heatmap and the video's background
-    string nom_video = "134823_Feng";
-    string heatmap_img = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png"; // where the heatmap will be saved
-    string background_img = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/134823_Feng_background.png";
-    
-    // Size of heat map. Must be the same that the video.
-    static const size_t w = 720, h = 720;
-    
-    // Create the heatmap object with the given dimensions (in pixel).
-    heatmap_t* hm = heatmap_new(w, h);
-    
-    //Mat result;
-    string line, pos_x, pos_y, time_msec, fps, nb_frames;
-    float ftime_msec, first_time, last_time;
-    size_t pos_tmp, pos_tmp2, pos_tmp3;
-    unsigned int x = 0, y = 0;
-    int i = 0;
-    
-    // Get the first time and last time of the txt positions file (x,y,t).
-    ifstream input(filename); // Path of the txt file
-    while (getline(input, line))
-    {
-        if (i == 0)// Get the first time of the txt file
-        {
-            pos_tmp = line.find(","); // ist comma
-            pos_tmp2 = line.find(",", pos_tmp + 1); // 2nd comma
-            pos_tmp3 = line.find(")");
-            time_msec = line.substr(pos_tmp2 + 1, ((pos_tmp3 - 1) - (pos_tmp2)));
-            ftime_msec = (stof(time_msec));
-            first_time = ftime_msec;
-        }
-        i++;
-    }
-    // Get the last time
-    pos_tmp = line.find(","); // 1st comma
-    pos_tmp2 = line.find(",", pos_tmp + 1); // 2nd comma
-    pos_tmp3 = line.find(")");
-    time_msec = line.substr(pos_tmp2 + 1, ((pos_tmp3 - 1) - (pos_tmp2)));
-    ftime_msec = (stof(time_msec));
-    last_time = ftime_msec;
-    cout << "First time : " << first_time << ", " << "Last time : "<<last_time << endl;
-    input.clear();
-    input.seekg(input.beg); // Go back to the begining of the file
-    
-    //Add points from the txt file to the heatmap
-    if (begin > last_time || begin < first_time)
-    {
-        cout << "Incorrect begin time (" << begin << ") : first time = " << first_time << ", last time = " << last_time << endl;
-    }
-    else
-    {
-        if (end > last_time || end < first_time)
-        {
-            cout << "Incorrect end time (" << end << ") : first time = " << first_time << ", last time = " << last_time << endl;
-        }
-        else
-        {
-            if (end<begin)
-            {
-                cout << "End time anterior at begin time" << endl;
-            }
-            else
-            {
-                while (getline(input, line))
-                {
-                    if (i>5)// 5 first points are not interesting -> never displayed on the heatmap
-                    {
-                        // Get x,y and time
-                        pos_tmp = line.find(","); // 1st coma
-                        pos_x = line.substr(1, pos_tmp - 1);
-                        pos_tmp2 = line.find(",", pos_tmp + 1); // 2nd coma
-                        pos_y = line.substr(pos_tmp + 1, ((pos_tmp2 - 1) - (pos_tmp)));
-                        pos_tmp3 = line.find(")");
-                        x = atoi(pos_x.c_str());
-                        y = atoi(pos_y.c_str());
-                        time_msec = line.substr(pos_tmp2 + 1, ((pos_tmp3 - 1) - (pos_tmp2)));
-                        ftime_msec = (stof(time_msec));
-                        
-                        if (ftime_msec >= begin && ftime_msec <= end)
-                        {
-                            heatmap_add_point(hm, x, y);
-                        }
-                    }
-                    i++;
-                }
-                // This creates an image out of the heatmap.
-                // `image` now contains the image data in 32-bit RGBA.
-                vector<unsigned char> image(w*h * 4);
-                //heatmap_render_default_to(hm, &image[0]);
-                heatmap_render_to(hm, heatmap_cs_b2w, &image[0]);
-                // Now that we've got a finished heatmap picture, we don't need the map anymore.
-                heatmap_free(hm);
-                
-                
-                // Finally, we use the lodepng library to save it as an image.
-                if (unsigned error = lodepng::encode(heatmap_img, image, w, h)) {
-                    cerr << "encoder error " << error << ": " << lodepng_error_text(error) << endl;
-                }
-            }
-        }
-    }
-}
 
 
 
