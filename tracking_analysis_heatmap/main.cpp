b18289c93b17cf10ea6ba34db556a26205135ad7
konovagit <corentincorde@orange.fr>
2015-06-10 15:30:40 +0100

    add branch create_heatmap

--- tracking_analysis_heatmap/main.cpp
+++ tracking_analysis_heatmap/main.cpp
@@ -27,6 +27,9 @@ IplImage* histogram(IplImage *image);
 
 double getPSNR(const Mat& I1, const Mat& I2); //for the comparison
 
+void create_histogram(string path_save, string image);
+
+
 
 int main()
 {
@@ -43,6 +46,8 @@ int main()
     
     conversion_grayscale(imageName2);
     
+    create_histogram("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/histogramme.png", "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png");
+
     
     
     return 0;
@@ -171,5 +176,78 @@ void generate_heatmap(string filename, double begin, double end)
         }
     }
 }
+
+void create_histogram(string path_save, string image)
+{
+    
+    Mat src, dst;
+    
+    /// Load image
+    src = imread( image, 1 );
+    
+    /* /todo
+     if( !src.data )
+     { return -1; }
+     */
+    
+    /// Separate the image in 3 places ( B, G and R )
+    vector<Mat> bgr_planes;
+    split( src, bgr_planes );
+    
+    /// Establish the number of bins
+    int histSize = 256;
+    
+    /// Set the ranges ( for B,G,R) )
+    float range[] = { 0, 256 } ;
+    const float* histRange = { range };
+    
+    bool uniform = true; bool accumulate = false;
+    
+    Mat b_hist, g_hist, r_hist, gray_hist;
+    
+    /// Compute the histograms:
+    calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+    
+    // Draw the histograms for B, G and R
+    int hist_w = 512; int hist_h = 400;
+    int bin_w = cvRound( (double) hist_w/histSize );
+    
+    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+    
+    /// Normalize the result to [ 0, histImage.rows ]
+    
+    normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    
+    /// Draw for each channel
+    for( int i = 1; i < histSize; i++ )
+    {
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+             Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+             Scalar( 255, 0, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+             Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+             Scalar( 0, 255, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+             Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+             Scalar( 0, 0, 255), 2, 8, 0  );
+        
+    }
+    
+    /// Display
+    namedWindow("Histogramme", CV_WINDOW_AUTOSIZE );
+    imshow("Histogramme", histImage );
+    
+    waitKey(0);
+    
+    //save histogram
+    imwrite( path_save, histImage );
+}
+
+
+
 
 
