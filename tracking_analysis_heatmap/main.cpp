233c456480b18da1b9942a7be3a00b46ad9c0038
konovagit <corentincorde@orange.fr>
2015-06-12 11:50:21 +0100

    refresh for opencv 2.4.11

--- tracking_analysis_heatmap/main.cpp
+++ tracking_analysis_heatmap/main.cpp
@@ -6,8 +6,8 @@
 #include "heatmap.h"
 #include "gray.h"
 
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/videoio/videoio.hpp>
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/videoio/videoio.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
