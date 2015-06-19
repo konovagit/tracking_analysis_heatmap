32b03ee1c091336325cb8ce6a02887eb3af37d1d
konovagit <corentincorde@orange.fr>
2015-06-19 10:31:30 +0100

    creation of new branch

--- tracking_analysis_heatmap/main.cpp
+++ tracking_analysis_heatmap/main.cpp
@@ -19,116 +19,36 @@
 using namespace std;
 using namespace cv;
 
-void generate_heatmap(string filename, double begin, double end);
-
-void conversion_grayscale(string png_name);
-
-IplImage* histogram(IplImage *image);
-
-double getPSNR(const Mat& I1, const Mat& I2); //for the comparison
-
-void create_histogram(string path_save, string image);
-
 
 
 int main()
 {
-    /*
-    string vide_name="134823_Feng";
-    string coordinates="../Res/Video_" + vide_name + "/" + vide_name + "_coordinates.txt";
-    cout<<"------generation heatmas----"<<endl;
-    cout<<"close windows to continue" <<endl;
-    
-    
-    generate_heatmap("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/134823_Feng_coordinates.txt", 5, 439);
-    string imageName = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png";
-    string imageName2 = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap9.png";
-    
-    
-    conversion_grayscale(imageName2);
-    
-    create_histogram("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/histogramme.png", "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png");
-    */
-    
-    /*
-     vector<uchar> array;
-     Mat image = imread( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png", 1 ); //grayscale equalized
-     array.assign(image.datastart, image.dataend);
-    Mat src_base, hsv_base;
-    Mat src_test1, hsv_test1;
-    Mat src_test2, hsv_test2;
-    Mat hsv_half_down;
-    
-    /// Load three images with different environment settings
- 
-    hsv_base = imread( "/Users/konova/tracking_analysis_heatmap/Res/heatmap_eq.png", 1 ); //grayscale equalized
-    hsv_test1 = imread("/Users/konova/tracking_analysis_heatmap/Res/heatmap_eq4R25_X.png", 1 );
-    hsv_test2 = imread("/Users/konova/tracking_analysis_heatmap/Res/heatmap_eq5R25_X.png", 1 );
-    
-    /// Convert to HSV
-   // cvtColor( src_base, hsv_base, COLOR_BGR2HSV );
-   // cvtColor( src_test1, hsv_test1, COLOR_BGR2HSV );
-   // cvtColor( src_test2, hsv_test2, COLOR_BGR2HSV );
-    
-    hsv_half_down = hsv_base( Range( hsv_base.rows/2, hsv_base.rows - 1 ), Range( 0, hsv_base.cols - 1 ) );
-    
-    /// Using 50 bins for hue and 60 for saturation
-    int h_bins = 50; int s_bins = 60;
-    int histSize[] = { h_bins, s_bins };
-    
-    // hue varies from 0 to 179, saturation from 0 to 255
-    float h_ranges[] = { 0, 180 };
-    float s_ranges[] = { 0, 256 };
-    
-    const float* ranges[] = { h_ranges, s_ranges };
-    
-    // Use the o-th and 1-st channels
-    int channels[] = { 0, 1 };
-    
-    
-    /// Histograms
-    MatND hist_base;
-    MatND hist_half_down;
-    MatND hist_test1;
-    MatND hist_test2;
-    
-    /// Calculate the histograms for the HSV images
-    calcHist( &hsv_base, 1, channels, Mat(), hist_base, 2, histSize, ranges, true, false );
-    normalize( hist_base, hist_base, 0, 1, NORM_MINMAX, -1, Mat() );
-    
-    calcHist( &hsv_half_down, 1, channels, Mat(), hist_half_down, 2, histSize, ranges, true, false );
-    normalize( hist_half_down, hist_half_down, 0, 1, NORM_MINMAX, -1, Mat() );
-    
-    calcHist( &hsv_test1, 1, channels, Mat(), hist_test1, 2, histSize, ranges, true, false );
-    normalize( hist_test1, hist_test1, 0, 1, NORM_MINMAX, -1, Mat() );
-    
-    calcHist( &hsv_test2, 1, channels, Mat(), hist_test2, 2, histSize, ranges, true, false );
-    normalize( hist_test2, hist_test2, 0, 1, NORM_MINMAX, -1, Mat() );
-    
-    /// Apply the histogram comparison methods
-    for( int i = 0; i < 4; i++ )
-    {
-        int compare_method = i;    //0 Correlation //1 Chi-Square //2 Intersection //3 Bhattacharyya distance
-        double base_base = compareHist( hist_base, hist_base, compare_method );
-        double base_half = compareHist( hist_base, hist_half_down, compare_method );
-        double base_test1 = compareHist( hist_base, hist_test1, compare_method );
-        double base_test2 = compareHist( hist_base, hist_test2, compare_method );
-        
-        printf( " Method [%d] Perfect, Base-Half, Base-Test(1), Base-Test(2) : %f, %f, %f, %f \n", i, base_base, base_half , base_test1, base_test2 );
-    }
-    
-   :: printf( "Done \n" );*/
+   
     
     vector<int> array,array2,res;
     Mat image = imread( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/Equ_heatmap1.png", 1 );
-    //Mat image2 = imread( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/lena.png", 1 );
+    Mat image2 = imread( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/Equ_heatmap3.png", 1 );
     
     array.assign(image.datastart, image.dataend);
-    //array2.assign(image2.datastart, image2.dataend);
+    array2.assign(image2.datastart, image2.dataend);
     
+    for (vector<int>::iterator it = array.begin() ; it != array.end(); ++it)
+    {
+        if((*it)>0)
+        {
+            (*it)=1;
+        }
+    }
+    for (vector<int>::iterator it = array2.begin() ; it != array2.end(); ++it)
+    {
+        if((*it)>0)
+        {
+            (*it)=1;
+        }
+    }
     
-    int count;
-    
+    //int count;
+    /*
     ofstream fichier("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/comparison.txt", ios::out | ios::trunc);
     if(fichier)
     {
@@ -139,234 +59,28 @@ int main()
                     (*it)=1;
                 }
               
-                /*
+     
                 if((array[(*it)]-array2[(*it)])!=0)
                 {
                     count++;
                 }
-                 */
+     
                 
                 fichier<< (*it)<<endl;
                
             }
-        
-        
-    
             fichier.close();
-        
     }
     else cerr << "Impossible d'ouvrir le fichier !" << endl;
+    */
     
-    cout<<"Nombre de pixel différent:"<<count;
-    //vector<float> array2;
-    //array.assign((float*)image.datastart, (float*)image.dataend);
-    
-    //float resultat;
-    //array2.converTo(array2, CV_32F);
-    
+    //double resultat;
+
+    cout<<norm(array,array2);
     //resultat=compareHist(array, array, CV_COMP_BHATTACHARYYA);
     //printf("Resultat:%f",resultat);
     
    
     return 0;
 }
-
-void conversion_grayscale(string png_name)
-{
-    Mat image = imread(png_name, CV_LOAD_IMAGE_COLOR);
-    if(! image.data )
-    {
-        cout <<  "Could not open or find the image" << endl ;
-    }
-    // Create a new matrix to hold the gray image
-    Mat gray;
-    // convert RGB image to gray
-    cvtColor(image, gray, CV_BGR2GRAY);
-    imwrite( png_name, gray );
-}
-
-
-
-void generate_heatmap(string filename, double begin, double end)
-{
-    // To write on specific files for each couple (begin,end).
-    string b = to_string((int)begin), e = to_string((int)end);
-    
-    // Path of the heatmap and the video's background
-    string nom_video = "134823_Feng";
-    string heatmap_img = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png"; // where the heatmap will be saved
-    string background_img = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/134823_Feng_background.png";
-    
-    // Size of heat map. Must be the same that the video.
-    static const size_t w = 720, h = 720;
-    
-    // Create the heatmap object with the given dimensions (in pixel).
-    heatmap_t* hm = heatmap_new(w, h);
-    
-    //Mat result;
-    string line, pos_x, pos_y, time_msec, fps, nb_frames;
-    float ftime_msec, first_time, last_time;
-    size_t pos_tmp, pos_tmp2, pos_tmp3;
-    unsigned int x = 0, y = 0;
-    int i = 0;
-    
-    // Get the first time and last time of the txt positions file (x,y,t).
-    ifstream input(filename); // Path of the txt file
-    while (getline(input, line))
-    {
-        if (i == 0)// Get the first time of the txt file
-        {
-            pos_tmp = line.find(","); // ist comma
-            pos_tmp2 = line.find(",", pos_tmp + 1); // 2nd comma
-            pos_tmp3 = line.find(")");
-            time_msec = line.substr(pos_tmp2 + 1, ((pos_tmp3 - 1) - (pos_tmp2)));
-            ftime_msec = (stof(time_msec));
-            first_time = ftime_msec;
-        }
-        i++;
-    }
-    // Get the last time
-    pos_tmp = line.find(","); // 1st comma
-    pos_tmp2 = line.find(",", pos_tmp + 1); // 2nd comma
-    pos_tmp3 = line.find(")");
-    time_msec = line.substr(pos_tmp2 + 1, ((pos_tmp3 - 1) - (pos_tmp2)));
-    ftime_msec = (stof(time_msec));
-    last_time = ftime_msec;
-    cout << "First time : " << first_time << ", " << "Last time : "<<last_time << endl;
-    input.clear();
-    input.seekg(input.beg); // Go back to the begining of the file
-    
-    //Add points from the txt file to the heatmap
-    if (begin > last_time || begin < first_time)
-    {
-        cout << "Incorrect begin time (" << begin << ") : first time = " << first_time << ", last time = " << last_time << endl;
-    }
-    else
-    {
-        if (end > last_time || end < first_time)
-        {
-            cout << "Incorrect end time (" << end << ") : first time = " << first_time << ", last time = " << last_time << endl;
-        }
-        else
-        {
-            if (end<begin)
-            {
-                cout << "End time anterior at begin time" << endl;
-            }
-            else
-            {
-                while (getline(input, line))
-                {
-                    if (i>5)// 5 first points are not interesting -> never displayed on the heatmap
-                    {
-                        // Get x,y and time
-                        pos_tmp = line.find(","); // 1st coma
-                        pos_x = line.substr(1, pos_tmp - 1);
-                        pos_tmp2 = line.find(",", pos_tmp + 1); // 2nd coma
-                        pos_y = line.substr(pos_tmp + 1, ((pos_tmp2 - 1) - (pos_tmp)));
-                        pos_tmp3 = line.find(")");
-                        x = atoi(pos_x.c_str());
-                        y = atoi(pos_y.c_str());
-                        time_msec = line.substr(pos_tmp2 + 1, ((pos_tmp3 - 1) - (pos_tmp2)));
-                        ftime_msec = (stof(time_msec));
-                        
-                        if (ftime_msec >= begin && ftime_msec <= end)
-                        {
-                            heatmap_add_point(hm, x, y);
-                        }
-                    }
-                    i++;
-                }
-                // This creates an image out of the heatmap.
-                // `image` now contains the image data in 32-bit RGBA.
-                vector<unsigned char> image(w*h * 4);
-                //heatmap_render_default_to(hm, &image[0]);
-                heatmap_render_to(hm, heatmap_cs_b2w, &image[0]);
-                // Now that we've got a finished heatmap picture, we don't need the map anymore.
-                heatmap_free(hm);
-                
-                
-                // Finally, we use the lodepng library to save it as an image.
-                if (unsigned error = lodepng::encode(heatmap_img, image, w, h)) {
-                    cerr << "encoder error " << error << ": " << lodepng_error_text(error) << endl;
-                }
-            }
-        }
-    }
-}
-
-void create_histogram(string path_save, string image)
-{
-    
-    Mat src, dst;
-    
-    /// Load image
-    src = imread( image, 1 );
-    
-    /* /todo
-     if( !src.data )
-     { return -1; }
-     */
-    
-    /// Separate the image in 3 places ( B, G and R )
-    vector<Mat> bgr_planes;
-    split( src, bgr_planes );
-    
-    /// Establish the number of bins
-    int histSize = 256;
-    
-    /// Set the ranges ( for B,G,R) )
-    float range[] = { 0, 256 } ;
-    const float* histRange = { range };
-    
-    bool uniform = true; bool accumulate = false;
-    
-    Mat b_hist, g_hist, r_hist, gray_hist;
-    
-    /// Compute the histograms:
-    calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-    calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-    calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
-    
-    // Draw the histograms for B, G and R
-    int hist_w = 512; int hist_h = 400;
-    int bin_w = cvRound( (double) hist_w/histSize );
-    
-    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-    
-    /// Normalize the result to [ 0, histImage.rows ]
-    
-    normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    
-    /// Draw for each channel
-    for( int i = 1; i < histSize; i++ )
-    {
-        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-             Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-             Scalar( 255, 0, 0), 2, 8, 0  );
-        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-             Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-             Scalar( 0, 255, 0), 2, 8, 0  );
-        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-             Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-             Scalar( 0, 0, 255), 2, 8, 0  );
-        
-    }
-    
-    /// Display
-    namedWindow("Histogramme", CV_WINDOW_AUTOSIZE );
-    imshow("Histogramme", histImage );
-    
-    waitKey(0);
-    
-    //save histogram
-    imwrite( path_save, histImage );
-}
-
-
-
-
-
 
