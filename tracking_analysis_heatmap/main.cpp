bc6d20cecea87a32eeef96db53836d98acdd63aa
konovagit <corentincorde@orange.fr>
2015-06-19 10:32:19 +0100

    delete part vector

--- tracking_analysis_heatmap/main.cpp
+++ tracking_analysis_heatmap/main.cpp
@@ -50,7 +50,7 @@ int main()
     create_histogram("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/histogramme.png", "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png");
     */
     
-    /*
+    
      vector<uchar> array;
      Mat image = imread( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png", 1 ); //grayscale equalized
      array.assign(image.datastart, image.dataend);
@@ -117,55 +117,8 @@ int main()
         printf( " Method [%d] Perfect, Base-Half, Base-Test(1), Base-Test(2) : %f, %f, %f, %f \n", i, base_base, base_half , base_test1, base_test2 );
     }
     
-   :: printf( "Done \n" );*/
+    printf( "Done \n" );
     
-    vector<int> array,array2,res;
-    Mat image = imread( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/Equ_heatmap1.png", 1 );
-    //Mat image2 = imread( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/lena.png", 1 );
-    
-    array.assign(image.datastart, image.dataend);
-    //array2.assign(image2.datastart, image2.dataend);
-    
-    
-    int count;
-    
-    ofstream fichier("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/comparison.txt", ios::out | ios::trunc);
-    if(fichier)
-    {
-            for (vector<int>::iterator it = array.begin() ; it != array.end(); ++it)
-            {
-                if((*it)>0)
-                {
-                    (*it)=1;
-                }
-              
-                /*
-                if((array[(*it)]-array2[(*it)])!=0)
-                {
-                    count++;
-                }
-                 */
-                
-                fichier<< (*it)<<endl;
-               
-            }
-        
-        
-    
-            fichier.close();
-        
-    }
-    else cerr << "Impossible d'ouvrir le fichier !" << endl;
-    
-    cout<<"Nombre de pixel différent:"<<count;
-    //vector<float> array2;
-    //array.assign((float*)image.datastart, (float*)image.dataend);
-    
-    //float resultat;
-    //array2.converTo(array2, CV_32F);
-    
-    //resultat=compareHist(array, array, CV_COMP_BHATTACHARYYA);
-    //printf("Resultat:%f",resultat);
     
    
     return 0;
