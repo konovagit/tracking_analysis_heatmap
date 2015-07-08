904ebf7683e90060a9907d5d370d66e50a74101d
konovagit <corentincorde@orange.fr>
2015-07-08 10:26:27 +0100

    refresh master, delete unused function

--- tracking_analysis_heatmap/main.cpp
+++ tracking_analysis_heatmap/main.cpp
@@ -6,8 +6,6 @@
 #include "heatmap.h"
 #include "gray.h"
 
-//#include <opencv2/imgcodecs.hpp>
-//#include <opencv2/videoio/videoio.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
@@ -23,34 +21,21 @@ void generate_heatmap(string filename, double begin, double end);
 
 void conversion_grayscale(string png_name);
 
-IplImage* histogram(IplImage *image);
-
-
-void create_histogram(string path_save, string image);
-
-void equalization(string source, string destination);
-
-
 
 int main()
 {
-    string vide_name="134823_Feng";
-    string coordinates="../Res/Video_" + vide_name + "/" + vide_name + "_coordinates.txt";
-    cout<<"------generation heatmas----"<<endl;
-    cout<<"close windows to continue" <<endl;
-    
-    
-    generate_heatmap("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/134823_Feng_coordinates.txt", 5, 439);
-    string imageName = "/Users/konova/tracking_analysis_heatmap/Res/heatmap.png";
-    string imageName2 = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap9.png";
-    
-    
-    //conversion_grayscale(imageName2);
-    
-    //create_histogram("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/histogramme.png", "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png");
 
-    //equalization("/Users/konova/tracking_analysis_heatmap/Res/heatmap_color5.png", "/Users/konova/tracking_analysis_heatmap/Res/heatmap_eq5.png");
     
+    generate_heatmap("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/FeetLocations_bis.txt", 0, 2062);
+    
+    //Superposition//
+    Mat dst;
+    Mat src1 = imread("/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap_garden.png");
+    Mat src2 = imread("/Users/konova/tracking_analysis_heatmap/Res/heatmap.png");
+    addWeighted( src1, 0.5, src2, 0.5, 0.0, dst);
+    imwrite( "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/garden_heatmap_result.png", dst );
+    
+
     return 0;
 }
 
@@ -77,11 +62,11 @@ void generate_heatmap(string filename, double begin, double end)
     
     // Path of the heatmap and the video's background
     string nom_video = "134823_Feng";
-    string heatmap_img = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap.png"; // where the heatmap will be saved
-    string background_img = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/134823_Feng_background.png";
+    string heatmap_img = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/heatmap_garden.png"; // where the heatmap will be saved
+    string background_img = "/Users/konova/tracking_analysis_heatmap/Res/Video_134823_Feng/background_garden.png";
     
     // Size of heat map. Must be the same that the video.
-    static const size_t w = 720, h = 720;
+    static const size_t w = 1280, h = 720;
     
     // Create the heatmap object with the given dimensions (in pixel).
     heatmap_t* hm = heatmap_new(w, h);
@@ -163,8 +148,8 @@ void generate_heatmap(string filename, double begin, double end)
                 // This creates an image out of the heatmap.
                 // `image` now contains the image data in 32-bit RGBA.
                 vector<unsigned char> image(w*h * 4);
-                //heatmap_render_default_to(hm, &image[0]);
-                heatmap_render_to(hm, heatmap_cs_b2w, &image[0]);
+                heatmap_render_default_to(hm, &image[0]);
+                //heatmap_render_to(hm, heatmap_cs_b2w, &image[0]);
                 // Now that we've got a finished heatmap picture, we don't need the map anymore.
                 heatmap_free(hm);
                 
@@ -176,92 +161,5 @@ void generate_heatmap(string filename, double begin, double end)
             }
         }
     }
-}
-
-void create_histogram(string path_save, string image)
-{
-    
-    Mat src, dst;
-    
-    /// Load image
-    src = imread( image, 1 );
-    
-    /* /todo
-     if( !src.data )
-     { return -1; }
-     */
-    
-    /// Separate the image in 3 places ( B, G and R )
-    vector<Mat> bgr_planes;
-    split( src, bgr_planes );
-    
-    /// Establish the number of bins
-    int histSize = 256;
-    
-    /// Set the ranges ( for B,G,R) )
-    float range[] = { 0, 256 } ;
-    const float* histRange = { range };
-    
-    bool uniform = true; bool accumulate = false;
-    
-    Mat b_hist, g_hist, r_hist, gray_hist;
-    
-    /// Compute the histograms:
-    calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-    calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-    calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
-    
-    // Draw the histograms for B, G and R
-    int hist_w = 512; int hist_h = 400;
-    int bin_w = cvRound( (double) hist_w/histSize );
-    
-    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-    
-    /// Normalize the result to [ 0, histImage.rows ]
-    
-    normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    
-    /// Draw for each channel
-    for( int i = 1; i < histSize; i++ )
-    {
-        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-             Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-             Scalar( 255, 0, 0), 2, 8, 0  );
-        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-             Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-             Scalar( 0, 255, 0), 2, 8, 0  );
-        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-             Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-             Scalar( 0, 0, 255), 2, 8, 0  );
-        
-    }
-    
-    /// Display
-    namedWindow("Histogramme", CV_WINDOW_AUTOSIZE );
-    imshow("Histogramme", histImage );
-    
-    waitKey(0);
-    
-    //save histogram
-    imwrite( path_save, histImage );
-}
-
-
-
-void equalization(string source, string destination)
-{
-    Mat src, dst;
-    
-    /// Load image
-    src = imread( source, 1 );
-    
-    /// Convert to grayscale
-    cvtColor( src, src, CV_BGR2GRAY );
-    /// Apply Histogram Equalization
-    equalizeHist( src, dst );
-    
-    imwrite( destination, dst );
 }
 
